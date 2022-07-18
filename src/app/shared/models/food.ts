2fdf99f75265a035771d1619be170a6014103155
viettu2k <doanviettu2k@gmail.com>
2022-07-18 13:15:18 +0700

    adding foot model

---
+++ src/app/shared/models/food.ts
@@ -0,0 +1,11 @@
+export class Food {
+  id!: number;
+  price!: number;
+  name!: string;
+  favorite: boolean = false;
+  star: number = 0;
+  tags?: string[];
+  imageUrl!: string;
+  cookTime!: string;
+  origins!: string[];
+}
