282370c2f4177ce55425a9a1a52e877408eade30
viettu2k <doanviettu2k@gmail.com>
2022-07-18 13:27:25 +0700

    change food propertype

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Food } from 'src/app/shared/models/food';
 
 @Injectable({
   providedIn: 'root',
@@ -6,15 +7,85 @@ import { Injectable } from '@angular/core';
 export class FoodService {
   constructor() {}
 
-  getAll(): string[] {
+  getAll(): Food[] {
     return [
-      '/assets/food-1.jpg',
-      '/assets/food-2.jpg',
-      '/assets/food-3.jpg',
-      '/assets/food-4.jpg',
-      '/assets/food-5.jpg',
-      '/assets/food-6.jpg',
-      '/assets/food-7.jpg',
+      {
+        id: 1,
+        name: 'Pizza',
+        price: 10,
+        cookTime: '10-20',
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: 'assets/images/food-1.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 2,
+        name: 'Chicken Soup',
+        price: 11,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['india', 'china'],
+        star: 5.0,
+        imageUrl: 'assets/images/food-2.jpg',
+        tags: ['SlowFood', 'Soup'],
+      },
+      {
+        id: 3,
+        name: 'Pizza',
+        price: 10,
+        cookTime: '10-20',
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: 'assets/images/food-3.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 4,
+        name: 'Pizza',
+        price: 10,
+        cookTime: '10-20',
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: 'assets/images/food-4.jpg',
+        tags: ['FastFood', 'Fry', 'Lunch'],
+      },
+      {
+        id: 5,
+        name: 'Pizza',
+        price: 10,
+        cookTime: '10-20',
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: 'assets/images/food-5.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 6,
+        name: 'Pizza',
+        price: 10,
+        cookTime: '10-20',
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: 'assets/images/food-6.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 7,
+        name: 'Bruh',
+        price: 10,
+        cookTime: '15-30',
+        favorite: true,
+        origins: ['VN'],
+        star: 4.5,
+        imageUrl: 'assets/images/food-7.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
     ];
   }
 }
